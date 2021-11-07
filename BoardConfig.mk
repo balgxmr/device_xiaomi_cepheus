3a44b8cc26da6df62de245019fc98890288a56fb
kondors1995 <normandija1945@gmail.com>
2021-11-07 06:37:21 +0000

    Add Livedisplay 2.1
    Squashed commit of the following:
    
    commit 5738f26d98e6ba99f3ce750ee6923a182aba6d6a
    Author: carpe462 <carpe462@gmail.com>
    Date:   Sun Aug 15 04:14:45 2021 -0700
    
        Sepolicy: Add Livedisplay 2.1 rules
    
    commit 72f0ff96284db94efd162e7a7bbe2a7d43ceeab7
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Mar 14 11:29:05 2021 +0100
    
        raphael: livedisplay: Implement IAntiFlicker interface
    
        Change-Id: I1dbc6d8989a80fe7b06d4356aeb717417997a793
    
    commit 53cef1761199409eddbe63a43c04a08e8c25865e
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Mar 27 17:42:18 2021 +0100
    
        raphael: livedisplay: Uprev to 2.1
    
        Change-Id: I98b2ea16ed342339ee652d5cb79dcfd22f8c0a0d
    
    commit 39d6de25bfd2c5523f4c611b524b1543617ab84d
    Author: Felix <google@ix5.org>
    Date:   Mon Jun 24 10:46:26 2019 +0200
    
        raphael: Add interface info to .rc files
    
        Signed-off-by: Felix <google@ix5.org>
        Change-Id: I6d70bbdb66c3dce280bf6908c3750316a6f6cf70
    
    commit 1f562a028425ae905f3f79ee1f8c748c76564a99
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Fri Jun 26 10:13:55 2020 +0200
    
        raphael: livedisplay: Drop unnecessary nullptr checks
    
        * new operator would never return a nullptr.
    
        Change-Id: I3a34770d70b2da53ba3099fb7647991e5bde33b8
    
    commit c97bcae7e6585766c89dd8e56814f5398e630a66
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Thu Jun 25 15:31:38 2020 +0800
    
        raphael: livedisplay: Use modules from common implementation
    
        Change-Id: Iafb9f6a56187beb574ca2eb9a97fb9c7e72893ce
    
    commit 0f8609660b727a9374dd847b13155540d7089886
    Author: Steven Moreland <smoreland@google.com>
    Date:   Tue Oct 1 11:01:16 2019 -0700
    
        raphael: livedisplay: Remove libhwbinder/libhidltransport deps
    
        Since these were combined into libhidlbase.
    
        Bug: 135686713
        Test: build only (libhwbinder/libhidltransport are empty)
        Change-Id: I640a3f0e0839214523627bf0b59fd5ae9dd2c61c
    
    commit a357b66c410576fb92b83386337cff28a56e7ec9
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Wed Jan 23 10:25:44 2019 +0000
    
        raphael: Implement SunlightEnhancement LiveDisplay HAL
    
        Change-Id: I17b0215ad78d86989c146291552c5e6e7b6a689d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    vendor/evolution/config/device_framework_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_cepheus
 TARGET_RECOVERY_DEVICE_MODULES := libinit_cepheus
