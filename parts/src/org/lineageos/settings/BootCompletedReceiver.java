67cf546893a87cafaedfc51ed037497682aadbee
baalgx <bal.prxgm1407@gmail.com>
2022-06-02 00:16:19 -0500

    Revert "cepheus: Nuke DC Dimming"
    This reverts commit 84d2d55c1e2382494846f7d95e716b079c34f12d.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -33,6 +33,8 @@ import org.lineageos.settings.utils.FileUtils;
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_KEY = "dc_dimming";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_bl";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
@@ -44,6 +46,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         PendingResult pendingResult = goAsync();
         DiracUtils.initialize(context);
         DozeUtils.onBootCompleted(context);
+
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
 
 	pendingResult.finish();
     }
