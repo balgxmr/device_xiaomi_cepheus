30f508d87c2086a49a8537d78f00b38139ac25fd
baalgx <bal.prxgm1407@gmail.com>
2022-06-02 08:18:06 -0500

    Nuke fix for livedisplay

--- device.mk
+++ device.mk
@@ -288,15 +288,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_cepheus
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.cepheus
-
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
