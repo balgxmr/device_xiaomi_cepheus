7ef26a977205a82e31f614f6c7c3c5e6692b206d
Andrzej Perczak <linux@andrzejperczak.com>
2022-06-15 18:10:45 +0300

    cepheus: Use coral tuning for columbus feature
    Columbus uses crosshatch tuning as default, this should help
    with some false-positives.

--- device.mk
+++ device.mk
@@ -388,6 +388,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
+    
+# Quick Tap
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.columbus.model=tap7cls_coral.tflite
 
 # RCS
 PRODUCT_PACKAGES += \
