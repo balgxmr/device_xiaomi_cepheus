2b82faf493ae6429fb6e503f7e621f996a89128d
baalgx <bal.prxgm1407@gmail.com>
2022-07-06 14:52:56 -0500

    cepheus: Drop unnecesary face unlock flag
    Face unlock is already being built by default in Pixelish source.

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -25,7 +25,6 @@ $(call inherit-product, device/xiaomi/cepheus/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # PixelOS Stuff
-TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
