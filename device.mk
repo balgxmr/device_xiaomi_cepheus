389a9e5f3ac277b3e4a2aec2578da67a5bde42b1
Albert I <kras@raphielgang.org>
2022-09-25 19:28:41 -0500

    cepheus: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
+    
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # Init scripts
 PRODUCT_COPY_FILES += \
