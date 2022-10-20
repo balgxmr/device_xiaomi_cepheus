d8a8924912a7823c443cf826e6646410f5e936f7
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2022-10-20 18:23:55 -0500

    cepheus: Build aidl power stats * Fix for aidl power stats errors. * New updated legacy-um sepolicy solved the android.hardware.power@1.0::IPower/default error - Now with this we fixing both errors: BatteryStatsService: Unable to load Power Hal or power.stats HAL hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest. hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -391,8 +391,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
-    
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
