a4d521883db6a1ec41d34e8d5fbc8b30854634f9
basamaryan <basam.aryan@gmail.com>
2022-10-24 21:58:51 -0500

    cepheus: Include GrapheneCamera

--- device.mk
+++ device.mk
@@ -110,6 +110,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libshim_megvii
+    
+PRODUCT_PACKAGES += \
+    GrapheneCamera
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
