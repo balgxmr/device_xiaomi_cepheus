90437518e2fc1f76ef5cdd16a65e738d9071164e
balgxmr <jose@pixelos.net>
2024-01-01 21:15:51 -0500

    Revert "cepheus: audio: Use AOSP USB v2 audio HAL"
    * Fixes reboot when receiving a call with USB-C Type Headphones connected. Also
      causes instability with some dacs connected. Reported by @ SanctumSaturn and others on TG.
    
    This reverts commit 96c93cd9241dc92419f09b96882b35f0c7489b29.

--- device.mk
+++ device.mk
@@ -54,7 +54,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    audio.usbv2.default \
+    audio.usb.default \
     audio.r_submix.default
 
 PRODUCT_PACKAGES += \
@@ -72,7 +72,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
