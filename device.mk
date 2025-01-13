91676785b767b68729264afaed75e0b650f3d841
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    cepheus: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -387,6 +387,12 @@ PRODUCT_COPY_FILES += \
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
