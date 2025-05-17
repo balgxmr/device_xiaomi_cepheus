48d9e21c613900e411ae1d6776d7c261ffc5d693
¨raystef66¨ <s.vanbarel@gmail.com>
2025-05-17 12:04:03 +0200

    cepheus: move TARGET_INIT_VENDOR_LIB to soong_config
    * vendor/lineage/config/BoardConfigSoong.mk:90: warning: TARGET_INIT_VENDOR_LIB is deprecated, please migrate to soong_config_set,libinit,vendor_init_lib

--- device.mk
+++ device.mk
@@ -243,6 +243,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.power.rc
 
+$(call soong_config_set,libinit,vendor_init_lib,libinit_cepheus)
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
