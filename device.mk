b43d42a09d6fe536dcc4e5cb3c9ed4177b9f57f0
¨raystef66¨ <s.vanbarel@gmail.com>
2025-05-17 12:00:01 +0200

    cepheus: set TARGET_SURFACEFLINGER_UDFPS_LIB to soong_config
    * vendor/lineage/config/BoardConfigSoong.mk:84: warning: TARGET_SURFACEFLINGER_UDFPS_LIB is deprecated, please migrate to soong_config_set,surfaceflinger,udfps_lib

--- device.mk
+++ device.mk
@@ -187,6 +187,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3.vendor \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
+$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/xiaomi:libudfps_extension.xiaomi)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
