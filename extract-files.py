9797ad29ddd903f7316e4005a289f38860734972
tribual <tribual@web.de>
2025-07-07 13:49:55 +0200

    cepheus: correct extract-files.py
    Change-Id: Ic745a035ec0085792657614ef0dd1f1a1208e362

--- extract-files.py
+++ extract-files.py
@@ -14,10 +14,14 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    "device/xiaomi/cepheus",
-    "hardware/qcom-caf/sm8150",
-    "hardware/xiaomi",
-    "vendor/qcom/opensource/display",
+	"hardware/qcom-caf/sm8150",
+	"hardware/qcom-caf/wlan",
+	"hardware/xiaomi",
+	"vendor/qcom/opensource/dataservices",
+	"vendor/qcom/opensource/commonsys-intf/display",
+	"vendor/qcom/opensource/commonsys/display",
+	"vendor/qcom/opensource/display",
+	"vendor/qcom/common/system/telephony",
 ]
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
@@ -43,12 +47,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/hw/audio.primary.msmnile.so': blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
-    'vendor/lib/hw/audio.primary.cepheus.so': blob_fixup()
-        .binary_regex_replace(
-            b'/vendor/lib/liba2dpoffload.so',
-            b'liba2dpoffload_cepheus.so\x00\x00\x00\x00',
-        )
-
 }  # fmt: skip
 
 module = ExtractUtilsModule(
