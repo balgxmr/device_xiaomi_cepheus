b7cc745f844652caeb5579ae491260515e039e20
tribual <tribual@web.de>
2025-07-07 13:24:42 +0200

    cepheus: correct path to compatibility_matrix
    also add hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
    
    Change-Id: I76a8b0176cc62656ccdd8fde597fee144b00201d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common//compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
