64748686719d2ae10dfb16a0699e9908d9d7e89b
tribual <tribual@web.de>
2025-07-07 13:21:15 +0200

    cepheus: adapt extract-files.py

--- extract-files.py
+++ extract-files.py
@@ -28,36 +28,17 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
-        .regex_replace(r'libc\+\+\.so', 'libc29.so')
-        .remove_needed('libMegviiFacepp-0.5.2.so')
-        .remove_needed('libmegface.so')
-        .add_needed('libshim_megvii.so'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .remove_needed('android.hidl.base@1.0.so'),
-    'vendor/bin/mi_thermald': blob_fixup()
-        .regex_replace(r'%d/on', r'%d/..'),
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
-    'proprietary/vendor/bin/sensors.qti': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'proprietary/vendor/lib64/libsnsapi.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'proprietary/vendor/lib64/libsnsdiaglog.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'proprietary/vendor/lib64/libssc.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'proprietary/vendor/lib64/libwvhidl.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'proprietary/vendor/lib64/mediadrm/libwvdrmengine.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'proprietary/vendor/lib64/sensors.ssc.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'vendor/lib64/libwvhidl.so': blob_fixup()
-        .replace_needed('libcrypto.so', 'libcrypto-v34.so')
-        .add_needed('libcrypto_shim.so'),
-    'vendor/lib64/mediadrm/libwvdrmengine.so': blob_fixup()
-        .add_needed('libcrypto_shim.so'),
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+        .regex_replace(' +seclabel u:r:batterysecret:s0\n', ''),
+    ('vendor/lib/hw/audio.primary.cepheus.so','vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib/hw/audio.primary.cepheus.so': blob_fixup()
+        .binary_regex_replace(
+            b'/vendor/lib/liba2dpoffload.so',
+            b'liba2dpoffload_cepheus.so\x00\x00\x00\x00',
+        )
 }  # fmt: skip
 
 module = ExtractUtilsModule(
