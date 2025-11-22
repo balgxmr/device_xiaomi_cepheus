3d11c94a19ea127c6025d78b6787dde4958faddc
Jose Encalada <jose@pixelos.net>
2025-11-22 02:17:21 -0500

    cepheus: aosp -> custom
    Change-Id: I8ba333568028bfa468ba079dedf1e50924111b05

--- custom_cepheus.mk
+++ custom_cepheus.mk
@@ -22,24 +22,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# PixelOS flags
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+TARGET_SCREEN_WIDTH := 1080
 
 # Exclude QCOM powerhal manifest
 TARGET_PROVIDES_POWERHAL := true
 
 # Device identifier
-PRODUCT_NAME := aosp_cepheus
+PRODUCT_NAME := custom_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_CHARACTERISTICS := nosdcard
-
-# Recovery
-TARGET_USES_AOSP_RECOVERY := true
