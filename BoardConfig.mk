72f30d9971c4c96011194a906bf0cea89d7492fd
balgxmr <jose@pixelos.net>
2025-11-22 00:52:18 -0500

    cepheus: Remove leftover

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,8 +91,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/voltage/config/device_framework_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 # Init
 TARGET_RECOVERY_DEVICE_MODULES := libinit_cepheus
